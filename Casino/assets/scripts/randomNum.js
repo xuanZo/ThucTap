f8eca363e7959210e0049730fceee616db0de9a5
Xuan <xuannguyen71320@gmail.com>
2019-02-12 16:24:39 +0700

    xóa phần thừa

--- Casino/assets/scripts/randomNum.js
+++
@@ -1,8 +0,0 @@
-function randomNum() {
-    return {
-        newValue: function (min, max) {
-            return Math.floor(Math.random() * (max - min + 1)) + min;
-        }
-    };
-}
-// module.exports = RandomNum;
